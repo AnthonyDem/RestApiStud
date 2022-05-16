2612d2a87844bf49daac879897ff6bee965e8808
AnthonyDemidovich <thony.dem@gmail.com>
2022-05-16 22:01:01 +0300

    put delete

--- rest_api_video/settings.py
+++ rest_api_video/settings.py
@@ -53,6 +53,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'rest_api_video.urls'
 
+AUTH_USER_MODEL = 'video_hosting.User'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -119,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
